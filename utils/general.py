8eca8e4a246bb16dd0523d1f942a2a6ea859a33a
rishi <rishikesh2715@gmail.com>
2024-05-02 14:48:07 -0500

    deleted extra funtion

--- utils/general.py
+++ utils/general.py
@@ -40,27 +40,3 @@ def find_in_list(array: np.ndarray, search_list: List[int]) -> np.ndarray:
     else:
         return np.isin(array, search_list)
 
-
-def get_stream_frames_generator(rtsp_url: str) -> Generator[np.ndarray, None, None]:
-    """
-    Generator function to yield frames from an RTSP stream.
-
-    Args:
-        rtsp_url (str): URL of the RTSP video stream.
-
-    Yields:
-        np.ndarray: The next frame from the video stream.
-    """
-    cap = cv2.VideoCapture(rtsp_url)
-    if not cap.isOpened():
-        raise Exception("Error: Could not open video stream.")
-
-    try:
-        while True:
-            ret, frame = cap.read()
-            if not ret:
-                print("End of stream or error reading frame.")
-                break
-            yield frame
-    finally:
-        cap.release()
